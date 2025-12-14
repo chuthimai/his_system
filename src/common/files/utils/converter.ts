cbb4f7f70d923f4c4bdfd18b96d7921f41e957df
maichu <chuthimai18245@gmail.com>
2025-12-14 23:01:03 +0700

    Sửa converter chỗ khám chuyên khoa tên bác sĩ khám bệnh và bác sĩ chuyên khoa

--- src/common/files/utils/converter.ts
+++ src/common/files/utils/converter.ts
@@ -217,7 +217,7 @@ export function convertDataForSpecialReport(data: ServiceReport): any {
     conclusion: conclusion,
 
     performDoctor: performer.name,
-    specialDoctor: requester.name,
+    specialDoctor: performer.name,
   };
 }
 
